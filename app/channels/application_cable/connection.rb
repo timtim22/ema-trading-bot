d50c9d457b42f944a68d21fa9185249870befdc6
zainab <zainabalqazzaz@gmail.com>
2025-06-11 22:07:11 -0400

    Fix linting issues by removing trailing whitespace

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,18 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
-    
+
     def connect
       self.current_user = find_verified_user
       Rails.logger.info "ActionCable: User #{current_user.id} connected"
     end
-    
+
     def disconnect
       Rails.logger.info "ActionCable: User #{current_user&.id} disconnected"
     end
-    
+
     private
-    
+
     def find_verified_user
       # Try to find user from session (for web connections)
       if session.present? && session[:user_id]
@@ -20,14 +20,14 @@ module ApplicationCable
           return session_user
         end
       end
-      
+
       # Try to find user from cookies (for Devise)
       if cookies.signed[:user_id]
         if user = User.find_by(id: cookies.signed[:user_id])
           return user
         end
       end
-      
+
       # Try to find user from Devise warden session
       if cookies.encrypted.present?
         devise_session = cookies.encrypted["_ema_trading_bot_session"]
@@ -38,18 +38,18 @@ module ApplicationCable
           end
         end
       end
-      
+
       # For development/testing, allow connection without authentication
       if Rails.env.development? || Rails.env.test?
         Rails.logger.warn "ActionCable: No authenticated user found, using first user for development"
         return User.first
       end
-      
+
       # Reject connection if no user found
       Rails.logger.error "ActionCable: No authenticated user found, rejecting connection"
       reject_unauthorized_connection
     end
-    
+
     def session
       @session ||= begin
         session_key = Rails.application.config.session_options[:key]
