ff6a3c02e133d27239af3d82f8a2574e8cad4c99
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2025-06-20 21:45:24 +0500

    homepage ui changes

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,10 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["mobileMenu"]
+  static targets = ["mobileMenu", "userDropdown", "userDropdownMenu"]
 
   connect() {
     this.mobileMenuIsOpen = false
+    this.userDropdownIsOpen = false
+    
+    // Close dropdown when clicking outside
+    document.addEventListener('click', (e) => {
+      if (this.hasUserDropdownTarget && !this.userDropdownTarget.contains(e.target)) {
+        this.closeUserDropdown()
+      }
+    })
+  }
+
+  disconnect() {
+    document.removeEventListener('click', this.closeDropdownOnClickOutside)
   }
 
   toggleMobileMenu() {
@@ -12,8 +24,48 @@ export default class extends Controller {
     
     if (this.mobileMenuIsOpen) {
       this.mobileMenuTarget.classList.remove("hidden")
+      // Add slide-down animation
+      this.mobileMenuTarget.style.animation = 'slideDown 0.3s ease-out'
     } else {
-      this.mobileMenuTarget.classList.add("hidden")
+      // Add slide-up animation before hiding
+      this.mobileMenuTarget.style.animation = 'slideUp 0.3s ease-out'
+      setTimeout(() => {
+        this.mobileMenuTarget.classList.add("hidden")
+      }, 300)
+    }
+  }
+
+  toggleUserDropdown(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    
+    this.userDropdownIsOpen = !this.userDropdownIsOpen
+    
+    if (this.userDropdownIsOpen) {
+      this.openUserDropdown()
+    } else {
+      this.closeUserDropdown()
+    }
+  }
+
+  openUserDropdown() {
+    if (this.hasUserDropdownMenuTarget) {
+      this.userDropdownMenuTarget.classList.remove("hidden")
+      const button = this.element.querySelector('#user-menu-button')
+      if (button) {
+        button.setAttribute('aria-expanded', 'true')
+      }
+    }
+  }
+
+  closeUserDropdown() {
+    if (this.hasUserDropdownMenuTarget) {
+      this.userDropdownMenuTarget.classList.add("hidden")
+      const button = this.element.querySelector('#user-menu-button')
+      if (button) {
+        button.setAttribute('aria-expanded', 'false')
+      }
+      this.userDropdownIsOpen = false
     }
   }
 } 
